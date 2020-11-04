b0a2baff94bd3ffd3d5f84c9f65d9c36503b7c12
house_huang <richhuang076@gmail.com>
2020-11-04 09:02:20 +0800

    add page

---
+++ precache-manifest.4624346d746c0fc5ae7adbab7f854e6d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5834598a384000818cad",
+    "url": "/css/app.1d50eff9.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "4a2eb577adf255018b0da7083364f81e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8b5ea3d769e6cacab730",
+    "url": "/js/about.fe960957.js"
+  },
+  {
+    "revision": "5834598a384000818cad",
+    "url": "/js/app.abe1fff6.js"
+  },
+  {
+    "revision": "2a54fb83551372c5db31",
+    "url": "/js/chunk-vendors.328e0bde.js"
+  },
+  {
+    "revision": "4dd2751dfa44c648270334e74aa5aa10",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
